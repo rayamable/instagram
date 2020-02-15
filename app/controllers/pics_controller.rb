ea3aebea053a494e66453abf8d8a414a1f13ce88
Ray Amable <rayamable@gmail.com>
2020-02-15 03:06:06 +0000

    add edit, update, destroy and view files

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,40 +1,55 @@
 class PicsController < ApplicationController
 
-    before_action :find_pic, only: [:show, :edit, :update, :destroy]
+  before_action :find_pic, only: [:show, :edit, :update, :destroy, :upvote]
+  
 
-    def index        
+  def index
+    @pics = Pic.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @pic = Pic.new 
+  end
+
+  def create 
+    @pic = Pic.new(pic_params)
+
+    if @pic.save
+        redirect_to @pic, notice: "Yesss! It was posted!"
+    else
+        render 'new'
     end
+  end
 
-    def show       
+  def edit
+  end
+
+  def update
+    if @pic.update(pic_params)
+        redirect_to @pic, notice: "Congrats! Pic was updated!"
+    else
+        render 'edit'
     end
+  end
 
-    def new
-        @pic = Pic.new        
-    end
-
-    def create 
-        @pic = Pic.new(pic_params)
-
-        if @pic.save
-            redirect_to @pic, notice: "Yesss! It was posted!"
-        else
-            render 'new'
-        end
-    end
-
-    def edit
-    end
+  def destroy
+    @pic.destroy
+    redirect_to root_path
+  end
 
 
 
-    private 
+  private
 
-    def pic_params
-        params.require(:pic).permit(:tittle, :description)       
-    end
+  def pic_params
+    params.require(:pic).permit(:title, :description, :image)
+  end
 
-    def find_pic
-        @pic = Pic.find(params[:id])
-        
-    end
+  def find_pic
+    @pic = Pic.find(params[:id])
+  end 
+
 end
